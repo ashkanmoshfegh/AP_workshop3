d7759b69f9d40fe265c5758491193b3a5cbf074d
ashkan moshfegh <moshfeghashkan@gmail.com>
2023-04-10 18:28:08 +0430

    phonenumber

--- src/phonenumber.java
+++ src/phonenumber.java
@@ -1,3 +1,15 @@
 public class phonenumber {
-
+        private String countrycode=new String();
+        private String number=new String();
+        public   void setcountrycode(String countrycode){
+            this.countrycode=countrycode;
+        }
+        public  void setnumber(String number){
+            this.number=number;
+        }
+        public String toString() {
+            if (countrycode == null) countrycode = "-";
+            if (number == null) number = "-";
+            return "(" + countrycode + ") " + number;
+        }
 }
